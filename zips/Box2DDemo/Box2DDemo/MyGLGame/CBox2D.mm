00e21ff16e43b0faa1d47d21d898df06a92fd785
Thomas Bui <thomasbui198@gmail.com>
2021-03-24 10:34:21 -0700

    Extracted box2d

---
+++ zips/Box2DDemo/Box2DDemo/MyGLGame/CBox2D.mm
@@ -0,0 +1,232 @@
+//
+//  CBox2D.m
+//  MyGLGame
+//
+//  Created by Borna Noureddin on 2015-03-17.
+//  Copyright (c) 2015 BCIT. All rights reserved.
+//
+
+#include <Box2D/Box2D.h>
+#include "CBox2D.h"
+#include <OpenGLES/ES2/glext.h>
+#include <stdio.h>
+
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
+
+
+
+#pragma mark - Brick and ball physics parameters
+
+// Set up brick and ball physics parameters here:
+//   position, width+height (or radius), velocity,
+//   and how long to wait before dropping brick
+
+const float MAX_TIMESTEP = 1.0f/60.0f;
+const int NUM_VEL_ITERATIONS = 10;
+const int NUM_POS_ITERATIONS = 3;
+
+
+#pragma mark - Box2D contact listener class
+
+class CContactListener : public b2ContactListener
+{
+public:
+    void BeginContact(b2Contact* contact) {};
+    void EndContact(b2Contact* contact) {};
+    void PreSolve(b2Contact* contact, const b2Manifold* oldManifold)
+    {
+        b2WorldManifold worldManifold;
+        contact->GetWorldManifold(&worldManifold);
+        b2PointState state1[2], state2[2];
+        b2GetPointStates(state1, state2, oldManifold, contact->GetManifold());
+        if (state2[0] == b2_addState)
+        {
+            // Use contact->GetFixtureA()->GetBody() to get the body
+            // Call RegisterHit (assume CBox2D object is in user data)
+        }
+    }
+    void PostSolve(b2Contact* contact, const b2ContactImpulse* impulse) {};
+};
+
+
+#pragma mark - CBox2D
+
+@interface CBox2D ()
+{
+    // Box2D-specific objects
+    b2Vec2 *gravity;
+    b2World *world; // contains all the physics objects
+    b2BodyDef *groundBodyDef;
+    b2Body *groundBody;
+    b2PolygonShape *groundBox;
+    b2Body *theBrick, *theBall;
+    CContactListener *contactListener;
+    
+    // GL-specific variables
+    // You will need to set up 2 vertex arrays (for brick and ball)
+    GLKMatrix4 modelViewProjectionMatrix;
+
+    // You will also need some extra variables here
+}
+@end
+
+@implementation CBox2D
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        // create a world, which needs a gravity to init
+        // see https://box2d.org/documentation/md__d_1__git_hub_box2d_docs_hello.html
+        gravity = new b2Vec2(0.0f, -10.0f);
+        world = new b2World(*gravity);
+        
+        // For HelloWorld
+        // To create a body, we need:
+        // 1. a body with position, damping, etc..
+        // 2. use the world object to create the body
+        // 3. Defines fixture with a shape, friction, density, etc.. all
+        // needed attrs. Fixture == a thing that binds a rigidbody/body to
+        // a 2D shape. This allows the shape to go under collision with
+        // other shapes. 
+        // 4. Attach/create the fixture on the body
+
+        // step 1. Create a ground body.
+        // to do this, need a body definition (bodyDef)
+        groundBodyDef = NULL;
+        groundBody = NULL;
+        groundBox = NULL;
+
+        // For brick & ball sample
+        contactListener = new CContactListener();
+        world->SetContactListener(contactListener);
+
+        // Set up the brick and ball objects for Box2D
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    if (gravity) delete gravity;
+    if (world) delete world;
+    if (groundBodyDef) delete groundBodyDef;
+    if (groundBox) delete groundBox;
+    if (contactListener) delete contactListener;
+}
+
+-(void)Update:(float)elapsedTime
+{
+    // Check here if we need to launch the ball
+    //  and if so, use ApplyLinearImpulse() and SetActive(true)
+
+    // Check if it is time yet to drop the brick, and if so
+    //  call SetAwake()
+
+    // If the last collision test was positive,
+    //  stop the ball and destroy the brick
+
+    if (world)
+    {
+        while (elapsedTime >= MAX_TIMESTEP)
+        {
+            world->Step(MAX_TIMESTEP, NUM_VEL_ITERATIONS, NUM_POS_ITERATIONS);
+            elapsedTime -= MAX_TIMESTEP;
+        }
+        
+        if (elapsedTime > 0.0f)
+        {
+            world->Step(elapsedTime, NUM_VEL_ITERATIONS, NUM_POS_ITERATIONS);
+        }
+    }
+
+
+    glEnable(GL_DEPTH_TEST);
+
+    // Set up vertex arrays and buffers for the brick and ball here
+
+    // For now assume simple ortho projection since it's only 2D
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakeOrtho(0, 800, 0, 600, -10, 100);
+    GLKMatrix4 modelViewMatrix = GLKMatrix4Identity;
+    modelViewProjectionMatrix = GLKMatrix4Multiply(projectionMatrix, modelViewMatrix);
+}
+
+-(void)Render:(int)mvpMatPtr
+{
+    glClearColor(0, 0, 0, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glUniformMatrix4fv(mvpMatPtr, 1, 0, modelViewProjectionMatrix.m);
+
+    // Bind each vertex array and call glDrawArrays
+    //  for each of the ball and brick
+}
+
+-(void)RegisterHit
+{
+    // Set some flag here for processing later...
+}
+
+-(void)LaunchBall
+{
+    // Set some flag here for processing later...
+}
+
+
+
+-(void)HelloWorld
+{
+    groundBodyDef = new b2BodyDef;
+    groundBodyDef->position.Set(0.0f, -10.0f);
+    groundBody = world->CreateBody(groundBodyDef);
+    groundBox = new b2PolygonShape;
+    groundBox->SetAsBox(50.0f, 10.0f);
+    
+    groundBody->CreateFixture(groundBox, 0.0f);
+    
+    // Define the dynamic body. We set its position and call the body factory.
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world->CreateBody(&bodyDef);
+    
+    // Define another box shape for our dynamic body.
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+    
+    // Define the dynamic body fixture.
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    
+    // Set the box density to be non-zero, so it will be dynamic.
+    fixtureDef.density = 1.0f;
+    
+    // Override the default friction.
+    fixtureDef.friction = 0.3f;
+    
+    // Add the shape to the body.
+    body->CreateFixture(&fixtureDef);
+    
+    // Prepare for simulation. Typically we use a time step of 1/60 of a
+    // second (60Hz) and 10 iterations. This provides a high quality simulation
+    // in most game scenarios.
+    float32 timeStep = 1.0f / 60.0f;
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+    
+    // This is our little game loop.
+    for (int32 i = 0; i < 60; ++i)
+    {
+        // Instruct the world to perform a single step of simulation.
+        // It is generally best to keep the time step and iterations fixed.
+        world->Step(timeStep, velocityIterations, positionIterations);
+        
+        // Now print the position and angle of the body.
+        b2Vec2 position = body->GetPosition();
+        float32 angle = body->GetAngle();
+        
+        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+    }
+}
+
+@end
